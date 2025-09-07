3c7872a0d2b35f8131dd2419462f2dbb3c4db387
tiago.soft.dev <tiago.soft.dev@gmail.com>
2025-09-07 01:20:54 +0000

    correção do codigo

--- test/dart_test.dart
+++ test/dart_test.dart
@@ -43,6 +43,49 @@ void main() {
      expect(() => factorial(-3), throwsArgumentError);
    });
   });
-
   
+}
+
+
+
+bool isPrime(int number) {
+  if (number <= 1) return false;
+  for (int i = 2; i <= number ~/ 2; i++) {
+    if (number % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+int sumOfDigits(int number) {
+  if (number < 0) {
+    throw ArgumentError('Número não pode ser negativo');
+  }
+  int sum = 0;
+  while (number > 0) {
+    sum += number % 10;
+    number ~/= 10;
+  }
+  return sum;
+}
+
+void main() {
+  group('Testes de Número primo', () {
+    test('Número primo 7', () {
+      expect(isPrime(7), isTrue);
+    });
+    test('Número não primo 10', () {
+      expect(isPrime(10), isFalse);
+    });
+  });
+
+  group('Testes de Soma dos dígitos', () {
+    test('Soma dos dígitos de 123', () {
+      expect(sumOfDigits(123), equals(6));
+    });
+    test('Soma dos dígitos de -1', () {
+      expect(() => sumOfDigits(-1), throwsArgumentError);
+    });
+  });
 }
