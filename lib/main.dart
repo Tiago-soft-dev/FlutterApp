55d71fe0eeb2fc6a25a17125e23bcefa52435c2f
tiago.soft.dev <tiago.soft.dev@gmail.com>
2025-10-13 00:04:35 +0000

    semana8

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'components.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -69,7 +71,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _decrementCounter() {
     setState(() {
-     
       _counter--;
     });
   }
@@ -92,6 +93,40 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
+
+      drawer: Drawer(
+       child: SafeArea(
+         child: ListView(
+           padding: EdgeInsets.zero,
+           children: [
+             DrawerHeader(
+               decoration: BoxDecoration(
+                 color: Theme.of(context).colorScheme.primaryContainer,
+               ),
+               child: Align(
+                 alignment: Alignment.bottomLeft,
+                 child: Text(
+                   'Menu',
+                   style: Theme.of(context).textTheme.headlineSmall,
+                 ),
+               ),
+             ),
+             ListTile(
+               leading: const Icon(Icons.pages),
+               title: const Text('Componentes'),
+               onTap: () {
+                 Navigator.pop(context); // fecha o drawer
+                 Navigator.push(
+                   context,
+                   MaterialPageRoute(builder: (_) => const ComponentsPage()),
+                 );
+               },
+             ),
+           ],
+         ),
+       ),
+     ),
+
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
@@ -121,7 +156,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-        
     );
   }
 }
